133232cdf772784291e222c46c9a1b41ff17bd7d
atorcode <atorcode@gmail.com>
2023-06-22 01:37:52 -0700

    Modify getByText tests to use getByRole instead

--- __test__/components/hero/index.test.tsx
+++ __test__/components/hero/index.test.tsx
@@ -4,12 +4,18 @@ import { Hero } from "@/app/components/hero";
 
 test("Application name's text is correct", () => {
   render(<Hero />);
-  const headingElement = screen.getByText(/^bird watcher$/i);
+  const headingElement = screen.getByRole("heading", {
+    level: 1,
+    name: /^bird watcher$/i,
+  });
   expect(headingElement).toBeInTheDocument();
 });
 
 test("Tagline's text is correct", () => {
   render(<Hero />);
-  const headingElement = screen.getByText(/^where feathers tell tales$/i);
+  const headingElement = screen.getByRole("heading", {
+    level: 2,
+    name: /^where feathers tell tales$/i,
+  });
   expect(headingElement).toBeInTheDocument();
 });
